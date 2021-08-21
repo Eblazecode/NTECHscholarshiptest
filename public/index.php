940c00afb0a07dde99fe77e5aa2989f8b55d6c0b
ENOCH DANIEL <dynablaze@gmail.com>
2021-08-21 03:02:20 +0100

    file updated 2.0.1.5 Reverted to default exam mood

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@
 <header id="header">
 
 
-    <div class="uk-container uk-container-large uk-margin-large-bottom ">
+    <div class="uk-container uk-container-large ">
         <div class="uk-box-shadow-small uk-overflow-hidden uk-border-rounded uk-inline uk-background-center-center
 		uk-background-cover uk-background-norepeat uk-margin-small-left uk-margin-medium-top
 		 uk-background-blend-overlay uk-overlay-blend"
@@ -32,9 +32,8 @@
                                 <a class="uk-navbar-item uk-logo" href="index.php"><img src="img/NTECH%20PNG-01.png" width="80px"></a>
                                 N-TECH ICT
                                 Scholarship  2021</h1>
-                            <p class="uk-margin-small-bottom">The next batch of examination
-                                comes up on the 21st August, 2021</p>
-
+                            <p class="uk-margin-small-bottom">Acquire the most in demand skills in
+                                software engineering, data science, Networking etc </p>
                         </div>
                     </div>
                     <div class="uk-width-1-5@m">
@@ -45,7 +44,7 @@
                             <div class="uk-rating">
                                 <div><a class="uk-button uk-button-primary-preserve
                                  uk-button-large uk-width-1-1@m" href="#instruction"
-                                        style="background-color: purple" > Thank you ! </a></div>
+                                        style="background-color: purple" >Instructions </a></div>
                             </div>
                         </div>
                     </div>
@@ -55,7 +54,66 @@
     </div>
 </header>
 
+<div class="uk-section uk-margin-xlarge-bottom" id="instruction">
+    <div class="uk-container">
+        <div class="uk-grid-large" data-uk-grid>
+            <div class="uk-width-expand@m">
+                <div class="uk-article" id="about">
+                    <div class="uk-width-1-@m">
+                        <div data-uk-sticky="bottom: true; media: @m">
+                            <div class="uk-card uk-card-default uk-card-body uk-width-1-1 uk-border-rounded-large">
+                                <h3>Instructions</h3>
 
+                                <p class="uk-margin-remove">please abide by the following instructions :</p>
+
+
+                                <ul class="uk-list uk-margin-small-top">
+                                    <li><span class="uk-margin-small-right" data-uk-icon="mail"></span> Close all programs, including email on your browser</li>
+                                    <li><span class="uk-margin-small-right" data-uk-icon="clock"></span> You have 60 minutes for 50 questions. The time
+                                        at the top of your screen</li>
+                                    <li><span class="uk-margin-small-right" data-uk-icon="clock"></span> This timer will count down to the  0 seconds.
+                                        Once your time is up, the exam will submit itself automatically </li>
+                                    <li><span class="uk-margin-small-right" data-uk-icon="arrow-left"></span> There is a previous and next button on each question page.
+                                        Click the previous button to see the question before your current question. Click the next button
+                                        to see the question after your current question</li>
+
+                                    <li><span class="uk-margin-small-right" data-uk-icon="push"></span>There is a submit button at
+                                        the last question page
+                                             ensure to click on it when done</li>
+
+                                </ul>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-width-1-3@m">
+                <div data-uk-sticky="offset: 100; bottom: true; media: @m">
+                    <div class="uk-card uk-card-default uk-card-body uk-width-1-1 uk-border-rounded-large">
+                        <span class="uk-margin-small-right" data-uk-icon="clock"></span>
+                        <h3>Duration</h3>
+                        <p class="uk-margin-remove">your session start and ends;</p>
+                        <ul class="uk-list uk-margin-small-top">
+                            <li> <H3>start : 9:00 AM</H3></li>
+                            <li> <H3>ends : 10:00 AM</H3></li>
+
+                        </ul>
+                        <a href="https://extendedforms.io/form/9433ff26-94e5-4da9-ba11-6a1542aa1c38/login" class="uk-button
+                                 uk-button-primary-preserve uk-button-large uk-width-1-1"
+
+                           style="background-color: mediumorchid">start </a>
+
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
---
+++ public/index3.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+
+<!-- events.html  19 Nov 2019 03:39:34 GMT -->
+<!--  --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- / -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>N-TECH  | Scholarship Test 2021</title>
+    <link rel="shortcut icon" type="image/png" href="img/NTECH%20PNG-01.png" >
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css" />
+    <script src="js/uikit.js"></script>
+</head>
+
+<body class="uk-background-body">
+<header id="header">
+
+
+    <div class="uk-container uk-container-large uk-margin-large-bottom ">
+        <div class="uk-box-shadow-small uk-overflow-hidden uk-border-rounded uk-inline uk-background-center-center
+		uk-background-cover uk-background-norepeat uk-margin-small-left uk-margin-medium-top
+		 uk-background-blend-overlay uk-overlay-blend"
+             style="background-image: url(img/codingblue.jpg);">
+            <div class="uk-padding-large">
+                <div data-uk-grid >
+                    <div class="uk-width-expand@m uk-flex uk-flex-middle uk-light">
+                        <div>
+
+                            <h1 class="uk-heading-small uk-letter-spacing-medium">
+                                <a class="uk-navbar-item uk-logo" href="index3.php"><img src="img/NTECH%20PNG-01.png" width="80px"></a>
+                                N-TECH ICT
+                                Scholarship  2021</h1>
+                            <p class="uk-margin-small-bottom">The next batch of examination
+                                comes up on the 21st August, 2021</p>
+
+                        </div>
+                    </div>
+                    <div class="uk-width-1-5@m">
+
+                    </div>
+                    <div class="uk-width-1-3@m uk-flex uk-flex-middle">
+                        <div class="uk-inline uk-light uk-border-rounded-large uk-box-shadow-small uk-overflow-hidden">
+                            <div class="uk-rating">
+                                <div><a class="uk-button uk-button-primary-preserve
+                                 uk-button-large uk-width-1-1@m" href="#instruction"
+                                        style="background-color: purple" > Thank you ! </a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+
+
+
+
+<?php include('includes/footer.php')?>
+
+<div id="offcanvas" data-uk-offcanvas="flip: true; overlay: true">
+    <div class="uk-offcanvas-bar">
+        <a class="uk-logo" href="index-2.html">N-TECH </a>
+        <button class="uk-offcanvas-close" type="button" data-uk-close="ratio: 1.2"></button>
+        <ul class="uk-nav uk-nav-primary uk-nav-offcanvas uk-margin-medium-top uk-text-center">
+            <li ><a href="#about">About</a></li>
+            <li ><a href="#courses">courses</a></li>
+            <li ><a href="#faqs">FAQs</a></li>
+            <li ><a href="#contact">contact</a></li>
+        </ul>
+        <div class="uk-margin-medium-top">
+            <a class="uk-button uk-width-1-1 uk-button-primary-light"
+               href="sign-up.html" style="background-color: mediumorchid; color: white">Register</a>
+        </div>
+        <div class="uk-margin-medium-top uk-text-center">
+
+        </div>
+    </div>
+</div>
+
+<script src="js/datepicker.js"></script>
+
+
+</body>
+
+
+<!-- events.html  19 Nov 2019 03:39:40 GMT -->
+</html>
